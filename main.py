59f185235fa9434022d1695efcd39633c5bbbe33
«Alex18» <alexburtsev18@yandex.ru>
2023-10-10 10:36:27 +0300

    crete file notes

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # beginning
+
+import json
+import datetime
+
+def load_notes():
+    try:
+        with open('notes.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
